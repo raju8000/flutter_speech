a411edd123863c7319d447898fee44f69f389967
Raju <rajum@rydotinfotech.com>
2020-07-01 20:08:06 +0530

    cleanup

---
+++ lib/main.dart
@@ -0,0 +1,284 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_recognition/speech_recognition.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: VoiceHome(),
+    );
+  }
+}
+
+class VoiceHome extends StatefulWidget {
+  @override
+  _VoiceHomeState createState() => _VoiceHomeState();
+}
+enum TtsState { playing, stopped, paused, continued }
+class _VoiceHomeState extends State<VoiceHome> {
+
+  FlutterTts flutterTts;
+  dynamic languages;
+  String language;
+  double volume = 1.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+  bool flag=false;
+
+  String _newVoiceText;
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+
+  get isStopped => ttsState == TtsState.stopped;
+
+  get isPaused => ttsState == TtsState.paused;
+
+  get isContinued => ttsState == TtsState.continued;
+
+
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+
+  String resultText = "";
+  Timer t;
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechRecognizer();
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _getLanguages();
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    if (kIsWeb || Platform.isIOS) {
+      flutterTts.setPauseHandler(() {
+        setState(() {
+          print("Paused");
+          ttsState = TtsState.paused;
+        });
+      });
+
+      flutterTts.setContinueHandler(() {
+        setState(() {
+          print("Continued");
+          ttsState = TtsState.continued;
+        });
+      });
+    }
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getLanguages() async {
+    languages = await flutterTts.getLanguages;
+    if (languages != null) setState(() => languages);
+  }
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        var result = await flutterTts.speak(_newVoiceText);
+        if (result == 1) setState(() => ttsState = TtsState.playing);
+      }
+    }
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  void initSpeechRecognizer() {
+    _speechRecognition = SpeechRecognition();
+
+    _speechRecognition.setAvailabilityHandler(
+          (bool result) => setState(() => _isAvailable = result),
+    );
+
+    _speechRecognition.setRecognitionStartedHandler(
+          () => setState(() => _isListening = true),
+    );
+setState(() {
+
+});
+    _speechRecognition.setRecognitionResultHandler(
+          (String speech) => setState(() /*(){*/ {
+            resultText=speech.toLowerCase();
+          }
+
+          /*}*/),
+    );
+
+    _speechRecognition.setRecognitionCompleteHandler((){
+      if(resultText=="what is my balance" || resultText=="my balance")
+      {resultText = _newVoiceText="your balance is 100\$";
+      }
+      else
+        resultText=_newVoiceText="I don't get it";
+
+
+      setState(() {
+        _isListening = false;
+        if (!_isListening && _newVoiceText.isNotEmpty)
+        {
+
+          t = Timer(Duration(seconds: 1), () {
+            flag=true;
+            _speak();
+            t.cancel();
+          });
+
+        }
+      });
+    }
+     /*     () => setState(()
+    {
+
+
+      if (!_isListening && resultText.isNotEmpty)
+        {
+
+       *//* Future.delayed(const Duration(milliseconds: 2000), () {*//*
+          if(resultText=="what is my balance" || resultText=="my balance")
+          {resultText = _newVoiceText="your balance is 100\$";
+          }
+          else
+            resultText=_newVoiceText="I don't get it";
+
+
+
+      *//*  });*//*
+//      _speak();
+    }
+          }),*/
+    );
+
+    _speechRecognition.activate().then(
+          (result) => setState(() => _isAvailable = result),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  child: Icon(Icons.cancel),
+                  mini: true,
+                  backgroundColor: Colors.deepOrange,
+                  onPressed: () {
+                    if (_isListening)
+                      _speechRecognition.cancel().then(
+                            (result) => setState(() {
+                          _isListening = result;
+                          resultText = "";
+                        }),
+                      );
+                  },
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.mic),
+                  onPressed: () {
+                    if (_isAvailable && !_isListening)
+                      _speechRecognition
+                          .listen(locale: "en_US")
+                          .then((result) => print('$result'));
+                  },
+                  backgroundColor: Colors.pink,
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.stop),
+                  mini: true,
+                  backgroundColor: Colors.deepPurple,
+                  onPressed: () {
+                    _stop();
+                    if (_isListening)
+                      _speechRecognition.stop().then(
+                            (result) => setState(() => _isListening = result),
+                      );
+                  },
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(
+                color: Colors.cyanAccent[100],
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 12.0,
+              ),
+              child: Text(
+                resultText,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
